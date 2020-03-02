c390daa96b699ab57be72f32926e1e87c670a01b
Xwilarg <xwilarg@yahoo.fr>
2020-03-02 19:17:34 +0100

    Add constexpr

--- Hanako/Hanako.ino
+++ Hanako/Hanako.ino
@@ -1,7 +1,7 @@
 #include <Stepper.h>
 
-const int steps = 2048;
-const int readyLed = 12;
+constexpr int steps = 2048;
+constexpr int readyLed = 12;
 Stepper motorLeft = Stepper(steps, 47, 51, 49, 53);
 Stepper motorRight = Stepper(steps, 46, 50, 48, 52);
 
